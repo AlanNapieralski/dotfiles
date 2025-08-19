b6a80abcec7aab0eabc630cab7d6a0a965e92d72
alannapieralski@numiko.com <alannapieralskinumiko.com@NMK-FM66523G72.local>
2025-08-19 16:05:53 +0100

    added a keymap for flote

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -79,3 +79,6 @@ vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
 
 vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
 vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+
+-- flote
+vim.keymap.set("n", "<leader>n", ":Flote<CR>", { desc = "Open Flote" })
